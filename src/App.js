e60266230f7d3df84d8a49cd46ae30834ecf130c
unknown <mattrob86@aol.com>
2020-02-26 20:44:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {Component} from 'react';
+import PlayerCard from './Playcard';
+
+class Game extends Component {
+constructor (){
+  super();
+  this.signs = ["rock", "scissors", "paper"];
+  this.state = {
+    playerOne: "rock",
+    playerTwo: "scissors",
+  }
+}
+
+
+render(){
+return (
+  <div className ="container">
+  <div>
+    <PlayerCard sign={this.state.playerOne}/>
+    <PlayerCard sign={this.state.playerTwo}/>
+  </div>
+  <div className ="winner">Here is the winner</div>
+  <button type="button">Play Game</button>
+  </div>
+);
+};
+
+export default Game;
---
+++ src/PlayerCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PlayerCard = (props) =>  {
+return (
+  <div className ="player-card"></div>
+
+)
+};
+
+export default PlayerCard;
